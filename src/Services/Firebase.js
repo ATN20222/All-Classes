e3daac79b77be0e9e0914cda5f540dbfd444f5a4
ATN20222 <antonabdalla30@gmail.com>
2025-02-24 10:12:50 +0200

    change firebase config

--- src/Services/Firebase.js
+++ src/Services/Firebase.js
@@ -6,16 +6,14 @@ import { setFCM } from "./AxiosApi";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCC4APmZVN5DjWIiydewwXASBXTAWQwXTU",
+    apiKey: "AIzaSyCuOTbxvFca1wSVTBh3tpuyRujGgd4JAsI",
     authDomain: "all-classes-497cb.firebaseapp.com",
-    databaseURL: "https://all-classes-497cb.firebaseio.com",
     projectId: "all-classes-497cb",
-    storageBucket: "all-classes-497cb.appspot.com",
-    messagingSenderId: "1084716750056",
-    appId: "1:1084716750056:web:0b6875fe30db7755e0e55d",
-    measurementId: "G-Y186M58YFT"
+    storageBucket: "all-classes-497cb.firebasestorage.app",
+    messagingSenderId: "356000361810",
+    appId: "1:356000361810:web:e61caf9234726144d95e8b",
+    measurementId: "G-H08FW0YDLC"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -26,7 +24,7 @@ const messaging = getMessaging(app);
 // Request permission for notifications
 export const requestNotificationPermission = async () => {
     try {
-        const token = await getToken(messaging, { vapidKey: "BCvPC_Z51qrtDEZs0sUf_3zXX0giALDB8sc02GCcZGGB6SMr7rlbFpZY_tsZ7D3wDxNxDdslrZxJ578WfBcPGQU" });
+        const token = await getToken(messaging, { vapidKey: "BGMt69a8Jborh3OYLnqJxRLrHph9XBtRDE2zbNMAdHIs8cs-8EYRVQRjHQDCqDESWFwi_I960nzvbj8Y3JvE_lY" });
         if (token) {
             console.log("FCM Token:", token);
             setFCM(token);
@@ -47,3 +45,6 @@ export const onMessageListener = () =>
             resolve(payload);
         });
     });
+
+
+
